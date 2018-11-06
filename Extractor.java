7e2aef9a37832183e13d726e80fec897df2bc7a4
zarifi <hoseinzarifi73@gmail.com>
2018-11-06 11:45:18 -0500

    parent finder added

--- Extractor.java
+++ Extractor.java
@@ -4,13 +4,27 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.sun.xml.internal.ws.util.StringUtils;
 
 
 public class Extractor {
 	
 	public ArrayList<String> finalList = new ArrayList<String>();	
+	
+	public ArrayList<String> javaFilePath = new ArrayList<String>();
 	// store the dependencies of current .java file based on import keywords
 	public void extract (String fromFilePath){
+		
+		String fromFileUrl = "";
+		Pattern pattern = Pattern.compile("hbase-2.1.0.*$");
+		Matcher matcher = pattern.matcher(fromFilePath);
+		if (matcher.find()){
+			//System.out.println("test");
+			fromFileUrl = matcher.group();
+		}
 		
 		try {
 			BufferedReader br = new BufferedReader(new FileReader(fromFilePath)); 
@@ -18,8 +32,14 @@ public class Extractor {
 			while ((line = br.readLine()) != null) {
 				if (line.contains("import org.apache")){
 			        String toFilePath = line.substring(7);
-			        toFilePath.replace(";", ".java");
-					finalList.add(fromFilePath+","+toFilePath);
+			        toFilePath = toFilePath.replace(";", ".java");			        
+			        toFilePath = toFilePath.replaceAll("[.]", "/");
+			        toFilePath = toFilePath.replace("/java", ".java");
+			        File toFileDir = new File(toFilePath);
+			        toFileDir = toFileDir.getAbsoluteFile();
+					File homeDir = new File("/Users/Mohammad/Documents/eecs4314/hbase-2.1.0");
+			        findParentDirectory(homeDir,toFileDir);
+					finalList.add(fromFileUrl+","+toFilePath);
 				}
 		    }
 		}
@@ -29,8 +49,21 @@ public class Extractor {
 	}
 	
 	
-	public ArrayList<String> storeDirectoryContentPath(File dir){
-		ArrayList<String> javaFilePath = new ArrayList<String>();
+	public void findParentDirectory(File dir,File targetPath){
+		
+		File[] files = dir.listFiles();
+		for (File file : files){
+			if (file.isDirectory())
+				//findParentDirectory(file, targetPath);
+			{
+				System.out.println(file.getName());
+				System.out.println(targetPath);
+			}
+		}
+		
+	}
+	
+	public void storeDirectoryContentPath(File dir){
 
 		try {
 			File[] files = dir.listFiles();
@@ -40,8 +73,11 @@ public class Extractor {
 				else
 				{
 					String currFilePath = file.getCanonicalPath();
-					if (currFilePath.contains(".java"))
-						javaFilePath.add(currFilePath);
+					//System.out.println(currFilePath);
+					if (currFilePath.contains(".java")){
+						//System.out.println(currFilePath);
+						javaFilePath.add(currFilePath.toString());
+					}
 				}
 			}
 		} 
@@ -49,11 +85,35 @@ public class Extractor {
 			System.out.println(e.getMessage());
 		}
 		
-		return javaFilePath;
+		//System.out.println(javaFilePath.size());
+		//return javaFilePath;
 	}
 	
 	
 	public static void main (String[] args){
+		Extractor test = new Extractor();
+		File homeDir = new File("/Users/Mohammad/Documents/eecs4314/hbase-2.1.0");
+		try {
+			test.storeDirectoryContentPath(homeDir);
+			ArrayList<String> urls = test.javaFilePath;
+			//System.out.println(test.javaFilePath.size());
+			for (int i = 0;i < urls.size();i++){
+				//System.out.println(urls.get(i));
+			}
+			
+			test.extract(urls.get(100));
+			//System.out.println(test.finalList.size());
+			for (int i = 0;i < test.finalList.size();i++){
+				//System.out.println(test.finalList.get(i));
+			}
+			
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		
 		
 		
 	}
